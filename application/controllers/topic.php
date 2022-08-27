d65096f29952d251032ebfd5259f38d736931952
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2022-08-27 22:36:29 +0900

    코드 이그나이터 시작

---
+++ application/controllers/topic.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Topic extends CI_Controller {
+    function index(){
+        echo '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8"/>
+            </head>
+            <body>
+                토픽 메인 페이지
+            </body>
+        </html>
+        ';
+    }
+    function get($id){
+        echo '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8"/>
+            </head>
+            <body>
+                토픽 '.$id.'
+            </body>
+        </html>
+        ';
+    }
+}
+?>
